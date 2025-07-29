4db0b8b5b9ce1a742adcf5116c7e5bdf02c4617a
Ishmael Akaboa <ishmaelakaboa@gmail.com>
2025-07-29 15:19:13 +0000

    post and get routes configured

--- models/url.go
+++ models/url.go
@@ -9,7 +9,7 @@ import (
 type Url struct {
 	ID        uint `gorm:"primaryKey"`
 	Original  string `gorm:"not null"`
-	Short     string `gorm:"uniqueIndex;not null"`
+	Short     string `gorm:"type:varchar(10);uniqueIndex"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
 	DeletedAt gorm.DeletedAt `gorm:"index"`
